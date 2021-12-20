81ef8bd15823bfcf85d82b5af7c59fe22847efc7
Michael Paukov <michael.paukov@gmail.com>
2021-12-20 12:05:51 +0100

    add library script

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -19,4 +19,28 @@ const makeGalleryItemMarkup = ({ preview, original, description }) => {
 const makeGalleryMarkup = galleryItems.map(makeGalleryItemMarkup).join('');
 
 const galleryElements = document.querySelector('.gallery');
+
+document
+  .querySelector('link[href="css/styles.css"]')
+  .insertAdjacentHTML(
+    'beforebegin',
+    `<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.css"/>`,
+  );
 galleryElements.insertAdjacentHTML('beforeend', makeGalleryMarkup);
+galleryElements.insertAdjacentHTML(
+  'afterend',
+  `<script src="https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js"></script>`,
+);
+
+const galleryLinkElements = document.querySelectorAll('.gallery__link');
+galleryLinkElements.forEach(galleryLinkElement => {
+  galleryLinkElement.addEventListener('click', e => e.preventDefault());
+});
+
+galleryElements.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(event) {
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+}
